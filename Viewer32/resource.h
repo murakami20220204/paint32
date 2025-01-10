728c1ded696276d80a74f3e91faa58884c4a7c23
murakami20220204 <98999589+murakami20220204@users.noreply.github.com>
2025-01-10 23:42:49 +0900

    メニューを編集

--- Viewer32/resource.h
+++ Viewer32/resource.h
@@ -4,25 +4,32 @@
 //
 #define IDS_APPTITLE                    101
 #define IDI_VIEWER                      101
-#define IDS_TOOLBAR                     102
 #define IDR_VIEWER                      101
+#define IDS_TOOLBAR                     102
 #define IDD_ABOUT                       105
 #define IDB_TOOLBAR                     107
 #define IDB_VIEWER16                    108
 #define IDC_VERSION                     1001
-#define ID_40001                        40001
-#define ID_40002                        40002
 #define IDM_OPEN                        40003
 #define IDM_EXIT                        40004
-#define ID_40005                        40005
 #define IDM_ABOUT                       40006
+#define IDM_ZOOMIN                      40013
+#define IDM_ZOOMOUT                     40014
+#define IDM_ZOOM                        40015
+#define IDM_TOOLBARWINDOW               40018
+#define IDM_STATUSWINDOW                40019
+#define IDM_OPENNEXT                    40022
+#define IDM_OPENPRIOR                   40023
+#define IDM_PRINT                       40025
+#define IDM_PAGEPRIOR                   40037
+#define IDM_PAGENEXT                    40038
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        109
-#define _APS_NEXT_COMMAND_VALUE         40009
+#define _APS_NEXT_COMMAND_VALUE         40041
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
